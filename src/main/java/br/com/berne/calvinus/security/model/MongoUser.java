d12773115a902a37d80e9926e0334960beb13a2a
Rafael Berne <rmberne@gmail.com>
2017-11-09 18:46:00 -0200

    Lombok removed

--- src/main/java/br/com/berne/calvinus/security/model/MongoUser.java
+++ src/main/java/br/com/berne/calvinus/security/model/MongoUser.java
@@ -1,14 +1,35 @@
 package br.com.berne.calvinus.security.model;
 
-import lombok.Data;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection = "users")
-@Data
 public class MongoUser {
 
   private String username;
   private String email;
   private String password;
 
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(String username) {
+    this.username = username;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(String email) {
+    this.email = email;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(String password) {
+    this.password = password;
+  }
 }
