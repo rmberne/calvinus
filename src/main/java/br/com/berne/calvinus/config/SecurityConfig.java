6fe4ff78f9d3eb13d7800bd9f62032e95d476e69
Rafael Berne <rmberne@gmail.com>
2017-11-09 18:35:56 -0200

    Custom authorities extractor for facebook

--- src/main/java/br/com/berne/calvinus/config/SecurityConfig.java
+++ src/main/java/br/com/berne/calvinus/config/SecurityConfig.java
@@ -2,19 +2,20 @@ package br.com.berne.calvinus.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.AuthoritiesExtractor;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.ResourceServerProperties;
 import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoTokenServices;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders
-    .AuthenticationManagerBuilder;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration
     .WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.oauth2.client.OAuth2ClientContext;
+import org.springframework.security.oauth2.client.OAuth2RestOperations;
 import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientAuthenticationProcessingFilter;
 import org.springframework.security.oauth2.client.filter.OAuth2ClientContextFilter;
@@ -24,6 +25,9 @@ import org.springframework.security.oauth2.config.annotation.web.configuration
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import java.util.List;
+import java.util.Map;
 
 import javax.servlet.Filter;
 
@@ -45,8 +49,23 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         new UserInfoTokenServices(facebookResource().getUserInfoUri(), facebook().getClientId());
     tokenServices.setRestTemplate(facebookTemplate);
     facebookFilter.setTokenServices(tokenServices);
+    tokenServices.setAuthoritiesExtractor(authoritiesExtractor(facebookTemplate));
     return facebookFilter;
   }
+
+  public AuthoritiesExtractor authoritiesExtractor(OAuth2RestOperations template) {
+    return map -> {
+      String url = (String) map.get("organizations_url");
+      @SuppressWarnings("unchecked")
+      List<Map<String, Object>> orgs = template.getForObject(url, List.class);
+      if (orgs.stream().anyMatch(org -> "spring-projects".equals(org.get("login")))) {
+        return AuthorityUtils.commaSeparatedStringToAuthorityList("ROLE_USER");
+      }
+      throw new BadCredentialsException("Not in Spring Team");
+    };
+  }
+
+
 
   @Bean
   @ConfigurationProperties("facebook.client")
